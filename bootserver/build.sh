a4e59470041303954af15b122df7b500aeaaf875
basilstotz <basil.stotz@gmail.ch>
2014-04-08 23:07:34 +0200

    ***

--- bootserver/build.sh
+++ bootserver/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#lb clean
-#lb build
+lb clean
+lb build
 
 WWW="www"
 
@@ -14,29 +14,37 @@ NEXT=$(( $THIS + 1 ))
 
 if test -z "$THIS"; then echo "oops!"; exit 1; fi
 
-if ! test -f $WWW/$(printf "%06i.img" $THIS); then
- THIS=$NEXT
-fi
-
-if ! test -f $WWW/$(printf "%06i.img" $PREV); then
-  PREV=$THIS
-fi
+cp binary/live/filesystem.squashfs $WWW/$(printf "%06i.img" $NEXT)
+cp binary.hybrid.iso $WWW/hybrid.iso
 
 
-cp binary/live/filesystem.squashfs ${WWW}/$(printf "%06i.img" $NEXT)
+
 
 if cd $WWW; then
+  if ! test -f $(printf "%06i.img" $THIS); then
+    THIS=$NEXT
+  fi
+
+  if ! test -f $(printf "%06i.img" $PREV); then
+    PREV=$THIS
+  fi
+
+  zsyncmake -u hybrid.iso hybrid.iso
   zsyncmake -u $(printf "%06i.img" $NEXT) $(printf "%06i.img" $NEXT)
+
 
   rm *-image-*
   touch old-image-$(printf "%06i" $PREV)
-  touch stable-image-$(printf "%06i" $THIS)
-  touch next-image-$(printf "%06i" $NEXT)
+  touch cur-image-$(printf "%06i" $THIS)
+  touch nxt-image-$(printf "%06i" $NEXT)
 
   #remove oldest
   if test -f $(printf "%06i.img" $PPREV); then
     rm $(printf "%06i.img*" $PPREV)
   fi
+
+  
+
   echo $NEXT > version
 fi
 
