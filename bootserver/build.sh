c2801e84a7e5eb6e90e524bfcf46a6ab4570453b
basilstotz <basil.stotz@gmail.com>
2014-04-12 17:24:32 +0200

    *

--- bootserver/build.sh
+++ bootserver/build.sh
@@ -14,33 +14,33 @@ NEXT=$(( $THIS + 1 ))
 
 if test -z "$THIS"; then echo "oops!"; exit 1; fi
 
-cp binary/live/filesystem.squashfs $WWW/$(printf "%06i.img" $NEXT)
-cp binary.hybrid.iso $WWW/hybrid.iso
+
+cp binary.hybrid.iso $WWW/$(printf "%06i.iso" $NEXT)
 
 
 
 
 if cd $WWW; then
-  if ! test -f $(printf "%06i.img" $THIS); then
+  if ! test -f $(printf "%06i.iso" $THIS); then
     THIS=$NEXT
   fi
 
-  if ! test -f $(printf "%06i.img" $PREV); then
+  if ! test -f $(printf "%06i.iso" $PREV); then
     PREV=$THIS
   fi
 
-  zsyncmake -u hybrid.iso hybrid.iso
-  zsyncmake -u $(printf "%06i.img" $NEXT) $(printf "%06i.img" $NEXT)
+
+  zsyncmake -u $(printf "%06i.iso" $NEXT) $(printf "%06i.iso" $NEXT)
 
 
   rm *-image-*
-  touch old-image-$(printf "%06i" $PREV)
-  touch cur-image-$(printf "%06i" $THIS)
-  touch nxt-image-$(printf "%06i" $NEXT)
+  touch old-iso-$(printf "%06i" $PREV)
+  touch cur-iso-$(printf "%06i" $THIS)
+  touch nxt-iso-$(printf "%06i" $NEXT)
 
   #remove oldest
-  if test -f $(printf "%06i.img" $PPREV); then
-    rm $(printf "%06i.img*" $PPREV)
+  if test -f $(printf "%06i.iso" $PPREV); then
+    rm $(printf "%06i.iso*" $PPREV)
   fi
 
   
