601bf4cc04005531cfd5d6df827eda4e7f57a1df
basilstotz <basil.stotz@gmail.com>
2014-04-15 12:46:29 +0200

    *

--- bootserver/build.sh
+++ bootserver/build.sh
@@ -1,52 +1,33 @@
 #!/bin/sh
 
-lb clean
-lb build
+ lb clean
+ lb build
 
-WWW="www"
+ WWW="www"
 
-if ! test -d $WWW; then mkdir $WWW;  fi
+ if ! test -d $WWW; then mkdir $WWW;  fi
 
-THIS=$(cat $WWW/version)
-PPREV=$(( $THIS - 2 ))
-PREV=$(( $THIS - 1 ))
-NEXT=$(( $THIS + 1 ))
+ THIS=$(cat $WWW/version)
+ if test -z "$THIS"; then echo "oops!"; exit 1; fi
 
-if test -z "$THIS"; then echo "oops!"; exit 1; fi
+ NEXT=$(( $THIS + 1 ))
+ 
 
+ cp binary.hybrid.iso $WWW/$(printf "%06i.iso" $NEXT)
+ cp binary.hybrid.iso $WWW/hybrid.iso
 
-cp binary.hybrid.iso $WWW/$(printf "%06i.iso" $NEXT)
-cp binary.hybrid.iso $WWW/hybrid.iso
-
-
-
-
-if cd $WWW; then
-  if ! test -f $(printf "%06i.iso" $THIS); then
-    THIS=$NEXT
-  fi
-
-  if ! test -f $(printf "%06i.iso" $PREV); then
-    PREV=$THIS
-  fi
+ if cd $WWW; then
+   zsyncmake -u $(printf "%06i.iso" $NEXT) $(printf "%06i.iso" $NEXT)
+   zsyncmake -u hybrid.iso hybrid.iso
 
 
-  zsyncmake -u $(printf "%06i.iso" $NEXT) $(printf "%06i.iso" $NEXT)
-  zsyncmake -u hybrid.iso hybrid.iso
+   #remove oldest
+   OLDEST=$(( $THIS - 2 ))
+   if test -f $(printf "%06i.iso" $OLDEST); then
+     rm $(printf "%06i.iso*" $OLDEST)
+   fi
 
-  rm *-iso-*
-  touch old-iso-$(printf "%06i" $PREV)
-  touch cur-iso-$(printf "%06i" $THIS)
-  touch nxt-iso-$(printf "%06i" $NEXT)
-
-  #remove oldest
-  if test -f $(printf "%06i.iso" $PPREV); then
-    rm $(printf "%06i.iso*" $PPREV)
-  fi
-
-  
-
-  echo $NEXT > version
+   echo $NEXT > version
 fi
 
 exit
